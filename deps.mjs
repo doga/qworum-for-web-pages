ee0eec7bd7ac933a0c1b5ed19502ebea2c50ef12
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2025-04-23 12:25:02 +0200

    add Qworum.getPersona(); untested.  dep version bump.

--- deps.mjs
+++ deps.mjs
@@ -1,4 +1,17 @@
-export { IRI, UniformResourceLocator } from 'https://esm.sh/gh/doga/IRI@1.4.5/mod.mjs';
+// export { IriParser, IRI, IRL, URN, iri, irl, url, urn } from 'https://esm.sh/gh/doga/IRI@3.1.3/mod.mjs';
+// export { IRI, UniformResourceLocator } from 'https://esm.sh/gh/doga/IRI@1.4.5/mod.mjs';
 
-import * as N3 from 'https://esm.sh/gh/doga/N3@1.18.2/mod.mjs';
+import * as N3 from 'https://esm.sh/gh/rdfjs/N3.js@v1.25.1/src/index.js';
+// import * as N3 from 'https://esm.sh/gh/doga/N3@1.18.2/mod.mjs';
 export {N3};
+
+import * as dataflow from 'https://esm.sh/gh/doga/rdf-dataflow@1.1.1/mod.mjs';
+export {dataflow};
+
+export { 
+  IriParser, IRI, iri, URN, urn, IRL, irl, url, 
+  Id, OrgId, orgid, GroupId, groupid, CollabId, collabid, UserId, userid, PersonaId, personaid,
+  Org, Group, PersonalGroup, Collab, Password, User,
+  Vcard, IndividualVcard, GroupVcard, OrgVcard, Name, Email, EmailUrl, Phone, PhoneUrl, Photo, Address, 
+  Persona, OrgPersona, GroupPersona, MemberRole, memberrole,
+} from 'https://esm.sh/gh/doga/qworum-domain-model@0.9.18/mod.mjs';
