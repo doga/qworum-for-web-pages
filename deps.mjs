a7215e66c9cacf9801ec3390d0c9dc2fd2c4b6f2
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2025-06-19 17:26:27 +0200

    update deps (partial pull-in of latest domain model lib)

--- deps.mjs
+++ deps.mjs
@@ -1,5 +1,9 @@
-// export { IriParser, IRI, IRL, URN, iri, irl, url, urn } from 'https://esm.sh/gh/doga/IRI@3.1.3/mod.mjs';
-// export { IRI, UniformResourceLocator } from 'https://esm.sh/gh/doga/IRI@1.4.5/mod.mjs';
+export { IriParser, IRI, IRL, URN, iri, irl, url, urn } from 'https://esm.sh/gh/doga/IRI@3.1.4/mod.mjs';
+
+// RDF
+import rdfTerm from 'https://esm.sh/gh/rdfjs/data-model@v2.1.0';
+import rdf from 'https://esm.sh/gh/rdfjs/dataset@v2.0.2';
+export { rdfTerm, rdf };
 
 import * as N3 from 'https://esm.sh/gh/rdfjs/N3.js@v1.25.2/src/index.js';
 // import * as N3 from 'https://esm.sh/gh/doga/N3@1.18.2/mod.mjs';
@@ -8,18 +12,4 @@ export {N3};
 import * as dataflow from 'https://esm.sh/gh/doga/rdf-dataflow@1.1.2/mod.mjs';
 export {dataflow};
 
-export { 
-  IriParser, IRI, iri, URN, urn, IRL, irl, url, 
-
-  Id, OrgId, GroupId, UserId, PasswordId, MembershipId, PartnershipId, PartnershipMembershipId, RoleId,
-  orgid, group_id, user_id, membership_id, partnership_id, partnership_membership_id, role_id,
-  bareorgid, baregroup_id, bareuser_id, barepartnership_id,
-
-  I18nText, Language,
-
-  Org, Group, PersonalGroup, Membership, Partnership, PartnershipMembership, Password, User, Role, wellKnownRoles,
-
-  Vcard, IndividualVcard, GroupVcard, OrgVcard, Name, Email, EmailUrl, Phone, PhoneUrl, Photo, Address, 
-
-  Persona
-} from 'https://esm.sh/gh/doga/qworum-domain-model@0.10.2/mod.mjs';
+export { Language } from "https://esm.sh/gh/doga/language@1.0.2/mod.mjs";
--- mod.mjs
+++ mod.mjs
@@ -1,10 +1,24 @@
 /**
- * Qworum library for web pages.
+ * JavaScript library for the frontends of Qworum services/APIs/classes.
  * @author Doğa Armangil <d.armangil@qworum.net>
- * @license Apache-2.0 <https://www.apache.org/licenses/LICENSE-2.0>
- * @see <https://qworum.net>
+ * @license  Apache-2.0 <https://www.apache.org/licenses/LICENSE-2.0>
+ * @see {@link https://qworum.net}
  */
 
+// domain model
+export { IriParser, IRI, IRL, URN, iri, irl, url, urn } from './deps.mjs';
+
+export { 
+  Id, OrgId, GroupId, UserId, PasswordId, MembershipId, PartnershipId, PartnershipMembershipId,
+  org_id, group_id, user_id, membership_id, partnership_id, partnership_membership_id,
+  bareorg_id, baregroup_id, bareuser_id, barepartnership_id,
+
+  Persona,
+
+  Role, Roleset, defaultRoleset,
+} from './lib/pulled-in/qworum-domain-model-0.15.0/mod.mjs';
+
+// runtime
 export {
   QworumScript, 
 
